834dd06609645285d6d72adc27366f20f84d39e3
yo16 <xxyo1xx@gmail.com>
2026-04-20 09:31:13 +0900

    [kakeizu-md9.3] 既存人物関係付けダイアログ実装
    - RelationDialog コンポーネント (親子/婚姻 ウィザード型 UI)
    - PersonSearchList サブコンポーネント (インクリメンタル検索)
    - YearMonthInput サブコンポーネント (婚姻の開始/終了年月)
    - createParentChild Server Action (認証 + ツリー所有権 + 重複/循環チェック)
    - createMarriage Server Action (認証 + ツリー所有権 + 重複チェック)
    - getPersonsByTree Server Action (ツリー内人物一覧取得)
    - Relation zod スキーマ (parent_child / marriage 両方)

---
+++ src/features/person/actions/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Person Server Actions の再エクスポート
+ */
+export { getPersonsByTree } from './get-persons-by-tree';
+export type { PersonSummary } from './get-persons-by-tree';
---
+++ src/features/relation/components/index.ts
@@ -0,0 +1,1 @@
+export { RelationDialog } from './RelationDialog';
